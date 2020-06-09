34df1ba7030caaf3b7a6c7ba76505d1b7b6bcf9d
oluwakeye-john <bjohnoluwakeye@gmail.com>
2020-06-10 00:03:13 +0100

    update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,15 +2,8 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { clearAll } from "../utils/saveList";
 
 const Navbar = ({ handleColorChange, defaultValue }) => {
-  const onClearAll = () => {
-    if (window.confirm("Are you sure you want to clear your todo")) {
-      clearAll();
-    }
-  };
-
   return (
     <nav className="navbar navbar-dark bg-dark shadow sticky-top">
       <div className="container">
@@ -27,12 +20,6 @@ const Navbar = ({ handleColorChange, defaultValue }) => {
             defaultValue={defaultValue}
           />
         </div>
-
-        {/* <div className="ml-auto">
-          <button className="btn" onClick={onClearAll}>
-            <span className="fas fa-trash text-white" />
-          </button>
-        </div> */}
       </div>
     </nav>
   );
